90d6a9e18513592131ad8fc71f0486d81b453f1d
KyongEE <nyyk0212@gmail.com>
2022-01-10 14:29:15 +0900

    comment commit

--- routes/posts.js
+++ routes/posts.js
@@ -1,18 +1,40 @@
-// routes/posts.js
-
 var express  = require('express');
 var router = express.Router();
 var Post = require('../models/Post');
+var User = require('../models/User');
+var Comment = require('../models/Comment');
 var util = require('../util');
 
 // Index
-router.get('/', function(req, res){
-  Post.find({})
-  .populate('author')
-  .sort('-createdAt')
-  .exec(function(err, posts){
-    if(err) return res.json(err);
-    res.render('posts/index', {posts:posts});
+router.get('/', async function(req, res){
+  var page = Math.max(1, parseInt(req.query.page));
+  var limit = Math.max(1, parseInt(req.query.limit));
+  page = !isNaN(page)?page:1;
+  limit = !isNaN(limit)?limit:10;
+
+  var skip = (page-1)*limit;
+  var maxPage = 0;
+  var searchQuery = await createSearchQuery(req.query);
+  var posts = [];
+
+  if(searchQuery) {
+    var count = await Post.countDocuments(searchQuery);
+    maxPage = Math.ceil(count/limit);
+    posts = await Post.find(searchQuery)
+      .populate('author')
+      .sort('-createdAt')
+      .skip(skip)
+      .limit(limit)
+      .exec();
+  }
+
+  res.render('posts/index', {
+    posts:posts,
+    currentPage:page,
+    maxPage:maxPage,
+    limit:limit,
+    searchType:req.query.searchType,
+    searchText:req.query.searchText
   });
 });
 
@@ -30,20 +52,28 @@ router.post('/', util.isLoggedin, function(req, res){
     if(err){
       req.flash('post', req.body);
       req.flash('errors', util.parseError(err));
-      return res.redirect('/posts/new');
+      return res.redirect('/posts/new'+res.locals.getPostQueryString());
     }
-    res.redirect('/posts');
+    res.redirect('/posts'+res.locals.getPostQueryString(false, { page:1, searchText:'' }));
   });
 });
 
 // show
 router.get('/:id', function(req, res){
-  Post.findOne({_id:req.params.id}) // 3
-    .populate('author')             // 3
-    .exec(function(err, post){      // 3
-      if(err) return res.json(err);
-      res.render('posts/show', {post:post});
-    });
+  var commentForm = req.flash('commentForm')[0] || {_id: null, form: {}};
+  var commentError = req.flash('commentError')[0] || {_id:null, parentComment:null, errors:{}};
+
+  Promise.all([
+    Post.findOne({_id:req.params.id}).populate({path:'author', select:'username'}),
+    Comment.find({post:req.params.id}).sort('createAt').populate({path:'author', select:'username'})
+  ])
+  .then(([post, comments]) => {
+    res.render('posts/show', {post:post, comments:comments, commentForm:commentForm, commentError:commentError});
+  })
+  .catch((err) => {
+    console.log('err : ', err);
+    return res.json(err);
+  });
 });
 
 // edit
@@ -69,9 +99,9 @@ router.put('/:id', util.isLoggedin, checkPermission, function(req, res){
     if(err){
       req.flash('post', req.body);
       req.flash('errors', util.parseError(err));
-      return res.redirect('/posts/'+req.params.id+'/edit');
+      return res.redirect('/posts/'+req.params.id+'/edit'+res.locals.getPostQueryString());
     }
-    res.redirect('/posts/'+req.params.id);
+    res.redirect('/posts/'+req.params.id+res.locals.getPostQueryString());
   });
 });
 
@@ -79,13 +109,13 @@ router.put('/:id', util.isLoggedin, checkPermission, function(req, res){
 router.delete('/:id', util.isLoggedin, checkPermission, function(req, res){
   Post.deleteOne({_id:req.params.id}, function(err){
     if(err) return res.json(err);
-    res.redirect('/posts');
+    res.redirect('/posts'+res.locals.getPostQueryString());
   });
 });
 
 module.exports = router;
 
-// private functions // 1
+// private functions
 function checkPermission(req, res, next){
   Post.findOne({_id:req.params.id}, function(err, post){
     if(err) return res.json(err);
@@ -93,4 +123,33 @@ function checkPermission(req, res, next){
 
     next();
   });
+}
+
+async function createSearchQuery(queries){
+  var searchQuery = {};
+  if(queries.searchType && queries.searchText && queries.searchText.length >= 3){
+    var searchTypes = queries.searchType.toLowerCase().split(',');
+    var postQueries = [];
+    if(searchTypes.indexOf('title')>=0){
+      postQueries.push({ title: { $regex: new RegExp(queries.searchText, 'i') } });
+    }
+    if(searchTypes.indexOf('body')>=0){
+      postQueries.push({ body: { $regex: new RegExp(queries.searchText, 'i') } });
+    }
+    if(searchTypes.indexOf('author!')>=0){
+      var user = await User.findOne({ username: queries.searchText }).exec();
+      if(user) postQueries.push({author:user._id});
+    }
+    else if(searchTypes.indexOf('author')>=0){
+      var users = await User.find({ username: { $regex: new RegExp(queries.searchText, 'i') } }).exec();
+      var userIds = [];
+      for(var user of users){
+        userIds.push(user._id);
+      }
+      if(userIds.length>0) postQueries.push({author:{$in:userIds}});
+    }
+    if(postQueries.length>0) searchQuery = {$or:postQueries};
+    else searchQuery = null;
+  }
+  return searchQuery;
 }
